0e2a8f67166e0a7ed4b663bef4befc28c1900f32
NathanielJamie <nathanzambranauchani@gmail.com>
2025-11-11 18:39:52 +0000

    lets go!

---
+++ script.js
@@ -0,0 +1,15 @@
+// Scroll reveal effect
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (elementTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
